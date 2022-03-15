f72e4a8c504defccc75bebe471db773c287cf77c
Gustavo Brandao <gustavojcbrandao@gmail.com>
2022-03-15 13:30:40 -0300

    first commit 2 att

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,7 +4,7 @@ import br.com.dio.model.Gato;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-/*        int a = 5;
+/*      int a = 5;
         int b = 3;
         System.out.println("Hello Java!" + (a+b));*/
 
